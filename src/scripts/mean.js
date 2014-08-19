e015ca3322bc373892d578573c61261d5c64338a
Livingston Martin <livingston@drmartin.com>
2014-08-19 11:28:37 -0700

    Browserify working with mean median and mode

---
+++ src/scripts/mean.js
@@ -0,0 +1,8 @@
+module.exports = function(numbers) {
+  var sum = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  var average = sum/numbers.length;
+  return average;
+};
